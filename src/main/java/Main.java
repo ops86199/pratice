cb7dc7d3305a6d1af9babe6a86069da27c424e5c
ops86199 <swainomprakash12@gmail.com>
2025-11-18 17:07:46 +0530

    update

--- src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Welcome to My Java App!");
-        System.out.println("This is running inside Docker.");
-    }
-}
